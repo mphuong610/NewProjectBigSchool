e7d37422ac5b937e71e47271ff0f958b65621854
Untitled <mphuong61097@gmail.com>
2019-03-30 17:41:38 +0700

    Add project files.

---
+++ BigSchoolPM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchoolPM.Startup))]
+namespace BigSchoolPM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
