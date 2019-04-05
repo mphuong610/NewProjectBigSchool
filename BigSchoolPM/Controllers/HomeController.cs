de13d3b2f2a3a8ad214fa0b71ad9e1c0c8cb89ce
Untitled <mphuong61097@gmail.com>
2019-04-05 23:38:31 +0700

    // AddNameColumn

--- BigSchoolPM/Controllers/HomeController.cs
+++ BigSchoolPM/Controllers/HomeController.cs
@@ -1,18 +1,17 @@
-﻿using System;
+﻿using BigSchoolPM.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace BigSchoolPM.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -25,6 +24,23 @@ namespace BigSchoolPM.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private ApplicationDbContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var upcomingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+
+            return View(upcomingCourses);
         }
     }
 }
